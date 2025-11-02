d0b3a0834db57207defa3f2b94482af4cf0cac17
Jeffreyman101 <17098923+jeffreyv101@users.noreply.github.com>
2025-11-02 14:00:28 -0500

    Removal of AI Panel (due to scheduling issues)

--- src/data/Events.js
+++ src/data/Events.js
@@ -14,13 +14,5 @@ export const events = [
         'startTime': '2025-09-13T09:00:00-04:00',
         'endTime': '2025-09-13T21:00:00-04:00',
         'location': 'School of Business',
-    },
-    {
-        'imageUrl': '/assets/events/aiPanel.jpg',
-        'title': 'Chat, are we cooked? (AI Panel)',
-        'description': 'Join us for a special event where we will be discussing the future of AI and its impact on software development. This event will feature many guest speakers from the industry.',
-        'startTime': '2025-11-03T18:00:00-05:00',
-        'endTime': '2025-11-03T19:30:00-05:00',
-        'location': 'SoBusi 2810'
     }
 ];
